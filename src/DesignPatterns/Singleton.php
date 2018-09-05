15247c22a06899426d2c52124bf52c3d7ad0730c
sireler <sireler@gmail.com>
2018-09-05 20:32:50 +0300

    Singleton pattern

---
+++ src/DesignPatterns/Singleton.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Sireler\DesignPatterns;
+
+/**
+ * Паттерн Singleton гарантирует, что у класса есть только один
+ * экземпляр, и предоставляет к нему глобальную точку доступа.
+ *
+ * Class Singleton
+ * @package Sireler\DesignPatterns
+ */
+class Singleton
+{
+    /**
+     * Экземпляр объекта
+     *
+     * @var
+     */
+    private static $instance;
+
+    /**
+     * Метод получения экземпляра
+     *
+     * @return mixed
+     */
+    public static function getInstance()
+    {
+        if (is_null(static::$instance)) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * Ограничение создания объекта
+     */
+    private function __construct() {}
+
+    /**
+     * Ограничение клонирования объекта
+     */
+    private function __clone() {}
+}
